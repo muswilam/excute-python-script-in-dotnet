8ab7101dcdba963db815722066e9ad04dcde6308
Sully <mohamed.swilam@dexef.net>
2023-02-13 17:38:57 +0200

    Execute python script in .Net using IronPython package

---
+++ PythonInDotNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using IronPython.Hosting;
+
+// create instance of python engine.
+var engine = Python.CreateEngine();
+
+// reading code from file.
+var source = engine.CreateScriptSourceFromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "calculator.py"));
+var scope = engine.CreateScope();
+
+// executing script in scope.
+source.Execute(scope);
+
+// get the value stored in scope.
+var classCalculator = scope.GetVariable("calculator");
+
+// initializing the class.
+var calculatorInstance = engine.Operations.CreateInstance(classCalculator);
+
+Console.WriteLine("Python: ");
+Console.WriteLine($"5 + 10 = {calculatorInstance.add(5, 10)}");
+Console.WriteLine($"5++ = {calculatorInstance.increment(5)}");
---
+++ PythonInDotNet/calculator.py
@@ -0,0 +1,7 @@
+﻿class calculator:
+    def add(self, x, y):
+        return x + y
+
+    def increment(self, x):
+        x += 1
+        return x
